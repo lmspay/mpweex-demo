57ddc9aafe1ff7995c61b2f464bfe00c3a41c487
ikantech <ikantech@163.com>
2020-03-05 15:07:44 +0800

    修改demo包名

--- MPWeexAndroid/app/src/main/java/com/lmspay/mpweexsdk/Constants.java
+++ MPWeexAndroid/app/src/main/java/com/lmspay/mpweexsdk/Constants.java
@@ -1,4 +1,4 @@
-package com.lmspay.mpweex.android;
+package com.lmspay.mpweexsdk;
 
 /**
  * Created by saint on 2019/7/29.
@@ -6,9 +6,9 @@ package com.lmspay.mpweex.android;
 
 public class Constants {
     // APPID
-    public static final String APPID = "your appid";
+    public static final String APPID = "APAAABbWxesSo_fYA41H4I";
     // 小程序API地址
-    public static final String MPHOST = "mpweex api host";
+    public static final String MPHOST = "https://zqmp.lmspay.com/zq-app/api";
 
     // 默认APP开户UID
     public static final String DEFAULT_APP_UID = "F1000002";
